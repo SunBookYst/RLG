9260a759fc854ca5d9f5a43b5788fd861cc3ab83
SummerFall1819 <summerfall1819@gmail.com>
2024-06-04 22:04:34 +0800

    (refactor) rewrite the utils and add proper docs.

--- subsenario/utils.py
+++ subsenario/utils.py
@@ -1,16 +1,16 @@
-from request.llmapi import initialize_llm
+from request.llmapi import initialize_llm, LLMAPI
 from request.stable_diffusion_request import StableDiffusion
 import json
 
 
 def read_file(prompt_name):
     """
-    读取文件内容。
+    read the file name.
     Args:
-        prompt_name (str): prompt文件的名称。
+        prompt_name (str): the file name of the prompt stored.
 
     Returns:
-        str: 文件内容。
+        str: the content of the file.
     """
     with open(f"../prompts/{prompt_name}.txt", 'r', encoding='utf-8') as file:
         prompt = file.read()
@@ -19,32 +19,38 @@ def read_file(prompt_name):
 
 def initialize_system():
     """
-    当玩家登入时直接调用
-    :return: DM、任务生成系统(task_generator)、背景生成系统(bg_generator)、绘图系统(sd)
+    The function to be called when the user is logged in.
+
+    Returns:
+        LLMAPI, LLMAPI, LLMAPI, StableDiffusion: The DM, task generator, background generator, and drawing system respectively.
     """
+
     # 初始化模型
     dm_prompt = read_file("DM")
-    DM = initialize_llm(dm_prompt)
     task_prompt = read_file("task")
-    task_generator = initialize_llm(task_prompt)
     bg_prompt = read_file('txt2img_background')
-    bg_generator = initialize_llm(bg_prompt)
-    sd = StableDiffusion()
+
+    DM            :LLMAPI          = initialize_llm(dm_prompt)
+    task_generator:LLMAPI          = initialize_llm(task_prompt)
+    bg_generator  :LLMAPI          = initialize_llm(bg_prompt)
+    sd            :StableDiffusion = StableDiffusion()
 
     return DM, task_generator, bg_generator, sd
 
 
 def task_generate(task_generator, task_type, description="任意"):
     """
-    根据描述生成一个任务
-    :param task_generator: 任务生成系统
-    :param task_director: 任务演绎系统
-    :param bg_generator: 背景生成系统
-    :param sd: 绘图系统
-    :param task_type: 任务类型
-    :param description: 玩家期望的任务描述
-    :return:dict{"task_name":（任务名称）,"task_description":（任务描述）,"attention":（注意事项）,"reward":（任务报酬）}
+    Generate a task based on the task type and description.
+
+    Args:
+        task_generator (LLMAPI): The task generator.
+        task_type (str): the type of the task
+        description (str, optional): The detailed description of the task. Defaults to "任意".
+
+    Returns:
+        dict{}: The information of generated task.
     """
+
     # 需求定制
     need = f"帮我生成一个{description}的{task_type}"
     task = task_generator.generateResponse(need, stream=True)
@@ -52,42 +58,63 @@ def task_generate(task_generator, task_type, description="任意"):
     return json.loads(task)
 
 
-def talk_to_dm(dm, player_input=None):
+def talk_to_dm(dm, player_input = None):
     """
-    用于跟dm对话
-    :param dm: 玩家对应的dm
-    :param player_input: 玩家的输入
-    :return: 系统的回复，dict{"status":（玩家状态）, "role":"系统", "text":（系统回复）}
+    Talk to the DM according to the player's input and to the player's ID.
+
+    Args:
+        dm (LLMAPI): The DM model.
+        player_input (str, optional): the player's input. Defaults to None.
+
+    Returns:
+        dict{}: The response from the DM.
     """
     return json.loads(dm.generateResponse(f'【玩家】{player_input}'))
 
 
 def task_init(play=None):
     """
-    初始化一个任务，在玩家决定游玩时使用
-    :param play: 任务综述
-    :return: 裁判(j), 任务演绎系统(task_director), 演绎系统的第一句话
+    Initialize the task when user selected.
+
+
+    Args:
+        play (str, optional): The overview of the task. Defaults to None.
+
+    Returns:
+        LLMAPI, LLMAPI, str: The judge system, the task director system, and the start statement of the task.
     """
     judge_prompt = read_file("judge")
-    j = initialize_llm(judge_prompt, True)
     act_prompt = read_file("task_acting")
-    task_director = initialize_llm(act_prompt)
-    start = task_director.generateResponse(play)
+
+
+    j            :LLMAPI = initialize_llm(judge_prompt, True)
+    task_director:LLMAPI = initialize_llm(act_prompt)
+    start        :str    = task_director.generateResponse(play)
 
     return j, task_director, start
 
 
 def task_play(task_director, j, player_input=None, player_status=None, last_play=None):
     """
-    与任务系统进行一轮对话
-    :param task_director:任务演绎系统
-    :param j:裁判
-    :param player_input:玩家的输入
-    :param player_status:玩家的状态
-    :param last_play:上次的play，便于judge系统判断。第一次时为start
-    :return: 任务系统的反馈(play), 游戏进程判断
+    Make a round of conversation with the task director.
+
+    Args:
+        task_director (LLMAPI): the task generator of the task.
+        j (LLMAPI): the judge of the task.
+        player_input (str, optional): the user's input. Defaults to None.
+        player_status (dict, optional): the user's current states. Defaults to None.
+        last_play (dict, optional): the last round of the task. Defaults to None.
+
+    Returns:
+        dict{} dict{}: The feedback of the task director, and to judge the task process.
     """
-    content = f'{"system":{last_play}, "player":{player_input}, "player_status":{player_status}}'
+
+    play_infos = {}
+    play_infos["system"] = last_play
+    play_infos["player"] = player_input
+    play_infos["player_status"] = player_status
+
+    content = json.dumps(play_infos)
     judge = json.loads(j.generateResponse(content))
     play = json.loads(task_director.generateResponse(f'【玩家】{player_input}'))
     return judge, play
