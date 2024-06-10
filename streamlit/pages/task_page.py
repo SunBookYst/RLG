515b1fcaf0f869a145dcc6e6af048e652ac4361d
SunBookYst <153164403+SunBookYst@users.noreply.github.com>
2024-06-10 23:59:12 +0800

    修复了任务无法顺利结束地bug

--- streamlit/pages/task_page.py
+++ streamlit/pages/task_page.py
@@ -5,6 +5,8 @@ import time
 
 url = "http://127.0.0.1:5000/"
 
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
 if 'condition' not in st.session_state:
     st.session_state.condition = 0
 if 'task' not in st.session_state:
@@ -13,6 +15,8 @@ if 'user_input' not in st.session_state:
     st.session_state.user_input = ""
 if 'task_list' not in st.session_state:
     st.session_state.task_list = []
+if 'focus_on_task' not in st.session_state:
+    st.session_state.focus_on_task = False
 
 
 def check_tasks():
@@ -56,24 +60,18 @@ def play_a_task(user_input):
     if response.status_code == 200:
         data = response.json()
         if 'reward' not in data:
-            return data['text'], 1
+            return data['text'], True
         else:
             st.write("恭喜你获得了：")
             st.write(data['reward'])
-            return data['text'], 0
+            return data['text'], False
     else:
         st.write("Error: Unable to communicate with the server.")
         return None
 
 
-def wait_for_task_initialize(task):
-    st.session_state.condition = 2
-    st.session_state.task = task
-
-
-def wait_for_task_acting(user_input):
-    st.session_state.condition = 3
-    st.session_state.user_input = user_input
+def end_task():
+    st.session_state.condition = 0
 
 
 if not st.session_state['logged_in']:
@@ -89,42 +87,53 @@ else:
             st.write("正在寻找任务...")
         else:
             for task in st.session_state['task_list']:
-                st.button(task, on_click=wait_for_task_initialize, args=[task])
+                if st.button(task):
+                    st.session_state.condition = 2
+                    st.session_state.task = task
+                    st.rerun()
 
     # 玩家此时正在游玩某个任务，进入与任务系统的对话界面
-    if st.session_state.condition == 1:
+    elif st.session_state.condition == 1:
         st.write(st.session_state['play'])
         user_input = st.chat_input("Say something")
         if user_input:
-            wait_for_task_acting(user_input)
+            st.session_state.condition = 3
+            st.session_state.user_input = user_input
+            st.rerun()
 
     # 玩家选择了某个任务，开始初始化
-    if st.session_state.condition == 2:
+    elif st.session_state.condition == 2:
         st.write("Initializing task...")
         time.sleep(0.5)
         st.session_state.condition = 4
         st.rerun()
 
     # 玩家提交了一段对话，等待系统回应
-    if st.session_state.condition == 3:
+    elif st.session_state.condition == 3:
         st.write("Please wait")
         time.sleep(0.5)
         st.session_state.condition = 5
         st.rerun()
 
-    if st.session_state.condition == 4:
+    elif st.session_state.condition == 4:
         play = select_task(st.session_state.task)
+        st.session_state.focus_on_task = True
         st.session_state.play = play
         st.session_state.condition = 1
         st.rerun()
 
-    if st.session_state.condition == 5:
-        st.session_state['play'], playing = play_a_task(st.session_state.user_input)
-        if playing:
-            st.session_state.condition = 1
+    elif st.session_state.condition == 5:
+        if st.session_state.focus_on_task:
+            st.session_state.play, st.session_state.focus_on_task = play_a_task(st.session_state.user_input)
+            if st.session_state.focus_on_task:
+                st.session_state.condition = 1
+                st.rerun()
+            else:
+                st.write(st.session_state.play)
+                if st.button("exit", on_click=end_task):
+                    st.session_state.condition = 0
+                    st.write("感谢您的努力，下次再会！")
+                    time.sleep(1.5)
+                    st.rerun()
 
-        else:
-            if st.button("exit"):
-                st.session_state.condition = 0
 
-        st.rerun()
